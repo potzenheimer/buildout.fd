dc9fd5874e695644e176039e8de461c91adcff3e
Christoph Boehner <cb@vorwaerts-werbung.de>
2012-07-04 00:54:10 +0200

    Media folder content type configuration

--- src/fd.sitecontent/fd/sitecontent/mediafolder.py
+++ src/fd.sitecontent/fd/sitecontent/mediafolder.py
@@ -1,31 +1,15 @@
 from five import grok
+from Acquisition import aq_inner
 from plone.directives import dexterity, form
 
-from zope import schema
-from zope.schema.interfaces import IContextSourceBinder
-from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
+from Products.CMFCore.utils import getToolByName
 
-from zope.interface import invariant, Invalid
-
-from z3c.form import group, field
-
-from plone.namedfile.interfaces import IImageScaleTraversable
-from plone.namedfile.field import NamedImage, NamedFile
-from plone.namedfile.field import NamedBlobImage, NamedBlobFile
-
-from plone.app.textfield import RichText
-
-from z3c.relationfield.schema import RelationList, RelationChoice
-from plone.formwidget.contenttree import ObjPathSourceBinder
-
-from fd.sitecontent import MessageFactory as _
+from plone.app.contentlisting.interfaces import IContentListing
 
 
-# Interface class; used to define content-type schema.
-
-class IMediaFolder(form.Schema, IImageScaleTraversable):
+class IMediaFolder(form.Schema):
     """
-    A medai folder that allows multi aploads of reusable files and images
+    A media folder that allows multi uploads of reusable files and images
     """
 
 
@@ -37,3 +21,15 @@ class View(grok.View):
     grok.context(IMediaFolder)
     grok.require('zope2.View')
     grok.name('view')
+
+    def update(self):
+        self.has_media = len(self.contained_files()) > 0
+
+    def contained_files(self):
+        context = aq_inner(self.context)
+        catalog = getToolByName(context, 'portal_catalog')
+        results = catalog(portal_type=['Image', 'File'],
+                          path=dict(query='/'.join(context.getPhysicalPath()),
+                                    depth=1))
+        resultlist = IContentListing(results)
+        return resultlist
