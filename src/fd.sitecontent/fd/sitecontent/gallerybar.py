d0c9a08ddb10b380abc993cc10030fccaed57ae6
Christoph Boehner <cb@vorwaerts-werbung.de>
2012-07-10 00:54:56 +0200

    Massive gallery view and viewlet cleanup

--- src/fd.sitecontent/fd/sitecontent/gallerybar.py
+++ src/fd.sitecontent/fd/sitecontent/gallerybar.py
@@ -1,7 +1,13 @@
 from five import grok
+from Acquisition import aq_inner
 from zope.interface import Interface
 from zope.component import getMultiAdapter
+
+from Products.CMFCore.utils import getToolByName
+
 from plone.app.layout.viewlets.interfaces import IPortalFooter
+
+from plone.app.blob.interfaces import IATBlobImage
 
 
 class GalleryBarViewlet(grok.Viewlet):
@@ -14,3 +20,40 @@ class GalleryBarViewlet(grok.Viewlet):
         pstate = getMultiAdapter((self.context, self.request),
                                  name="plone_portal_state")
         self.portal_url = pstate.portal_url()
+        self.has_images = len(self.images()) > 0
+
+    def images(self):
+        items = []
+        data = self._gallery_data()
+        for item in data:
+            info = {}
+            info['title'] = item.Title
+            info['url'] = item.getURL()
+            info['image_tag'] = self.getImageTag(item.getObject())
+            items.append(info)
+        return items
+
+    def _gallery_data(self):
+        context = aq_inner(self.context)
+        catalog = getToolByName(context, 'portal_catalog')
+        gallery = self._mediafolder()
+        results = catalog(object_provides=IATBlobImage.__identifier__,
+                          path=dict(query='/'.join(gallery.getPhysicalPath()),
+                                    depth=1))
+        return results
+
+    def _mediafolder(self):
+        pstate = getMultiAdapter((self.context, self.request),
+                                 name="plone_portal_state")
+        portal = pstate.portal()
+        gallery = portal['gallery']
+        return gallery
+
+    def getImageTag(self, item):
+        obj = item
+        scales = getMultiAdapter((obj, self.request), name='images')
+        scale = scales.scale('image', width='250', height='250')
+        imageTag = None
+        if scale is not None:
+            imageTag = scale.tag()
+        return imageTag
